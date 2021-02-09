304c3c6164873ecf6045e92a2935783439980bfd
AT <agtts25@gmail.com>
2021-02-09 01:12:38 -0500

    Added shuffle stocks functionality

--- game/game.js
+++ game/game.js
@@ -6,22 +6,40 @@ let stockName;
 let oldCost;
 let newCost;
 
+// Shuffle the stock placements on page
+function shuffle() {
 
-// When the 'invest' button is clicked, delete any previous winner/loser messages
+    let parent = document.querySelector(".container-fluid .row");
+    for (var i = 0; i < parent.children.length; i++) {
+        parent.appendChild(parent.children[Math.random() * i | 0]);
+    }
+}
+
+/* GAME FUNCTIONALITY SUMMARY
+   1. When the 'invest' button is clicked,
+   del() deletes previous winner/loser messages.
+   2. worth() calculates losses or wins
+   3. worth() determines win or lose message
+   4. worth() unchecks user's selection after 1/2 a sec
+   5. shuffle() re-arranges the stock options
+*/
+
 checkBtn.addEventListener('click', del);
 
+// After any previous messages are deleted, call worth()
 function del() {
     let oldMsg = document.getElementById('msgBlock');
     oldMsg.remove();
     worth();
 }
 
-/* Calculates if user is a winner or loser based on their stock's 
-return from opening day 2009 to February 9, 2021 */
+/*  Calculates if user is a winner or loser based on
+their stock's return from opening day 2009 to February 9, 2021.
+Worth calls shuffle() */
 function worth() {
 
     // Save the checkbox option the user checked
-    let checkbox = document.querySelectorAll('input[type=checkbox]:checked')
+    let checkbox = document.querySelectorAll('input[type=checkbox]:checked');
 
     /* Get the selected stock's name,
     its old cost,
@@ -43,7 +61,7 @@ function worth() {
 
     // End message
     let totalMsg = `Today you'd be worth $${finalAmount} 
-    had you invested in ${stockName}. <br>`
+    had you invested in ${stockName}.<br>`
 
     // Select the div that will contain the message
     let resultMsg = document.createElement('div');
@@ -58,7 +76,7 @@ function worth() {
     newDiv.className = 'response';
     newDiv.id = 'result';
 
-    // Append custom winner and loser messages
+    // Append custom winner or loser message
     resultMsg.appendChild(newDiv)
 
     if (todayWorth > startAmount) {
@@ -70,14 +88,14 @@ function worth() {
         resultMsg.innerHTML = `Loser! ${totalMsg}`;
     }
 
-    // After half a second, uncheck the checkbox
+    // After half a second, uncheck the checked stock
     setTimeout(function () {
         document
             .getElementById(stockName)
             .checked = false;
     }, 500);
+    setTimeout(function () {
+        shuffle();
+    }, 1000);
 
-   
 }
-
-
